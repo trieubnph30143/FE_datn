987000a7815b3bd9c408813d69878f9ecdaf3d5a
buivantoana <toanbui219@gmail.com>
2024-05-01 15:50:19 +0700

    ui footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,18 +6,20 @@ import { Button } from "@mui/material";
 const Footer = () => {
   return (
     <div>
-      <footer>
-        <div className="row">
-          <div className="col" style={{ width: "30%" }}>
-            <div className="footer-logo" style={{display: "flex", gap: "12px"}}>
+      <footer style={{ marginTop: "80px" }}>
+        <div className='row'>
+          <div className='col' style={{ width: "30%" }}>
+            <div
+              className='footer-logo'
+              style={{ display: "flex", gap: "12px" }}>
               <img
                 src={`${logo}`}
-                alt=""
+                alt=''
                 width={40}
                 style={{ borderRadius: "7px" }}
               />
-              <div style={{paddingTop: "10px"}}>
-                <span style={{ fontSize: "16px", fontWeight: "bold"}}>
+              <div style={{ paddingTop: "10px" }}>
+                <span style={{ fontSize: "16px", fontWeight: "bold" }}>
                   Học Lập Trình Để Đi Làm
                 </span>
               </div>
@@ -28,94 +30,109 @@ const Footer = () => {
                 fontSize: "15px",
                 color: "#ccc",
                 padding: "20px 0",
-              }}
-            >
+              }}>
               <div>Điện thoại: 0246.329.1102</div>
               <div>Email: contact@fullstack.edu.vn</div>
               <div>Số 11D, lô A10, khu đô thị Nam Trung</div>
               <div>Yên, Phường Yên Hòa, Quận Cầu Giấy, TP. Hà Nội</div>
             </div>
           </div>
-          <div className="col" style={{ width: "16.66667%" }}>
-            <div style={{ paddingTop: "12px", fontWeight: "bold", paddingBottom: "15px" }}>VỀ F8</div>
+          <div className='col' style={{ width: "16.66667%" }}>
+            <div
+              style={{
+                paddingTop: "12px",
+                fontWeight: "bold",
+                paddingBottom: "15px",
+              }}>
+              VỀ F8
+            </div>
             <ul>
               <li>
-                <a href="">Giới thiệu</a>
+                <a href=''>Giới thiệu</a>
               </li>
               <li>
-                <a href="">Liên hệ</a>
+                <a href=''>Liên hệ</a>
               </li>
               <li>
-                <a href="">Điều khoản</a>
+                <a href=''>Điều khoản</a>
               </li>
               <li>
-                <a href="">Bảo mật</a>
+                <a href=''>Bảo mật</a>
               </li>
               <li>
-                <a href="">Cơ hội việc làm</a>
+                <a href=''>Cơ hội việc làm</a>
               </li>
             </ul>
           </div>
-          <div className="col" style={{ width: "16.66667%" }}>
+          <div className='col' style={{ width: "16.66667%" }}>
             <div>
-              <div style={{ paddingTop: "50px", fontWeight: "bold", paddingBottom: "15px" }}>
+              <div
+                style={{
+                  paddingTop: "50px",
+                  fontWeight: "bold",
+                  paddingBottom: "15px",
+                }}>
                 SẢN PHẨM
               </div>
               <ul>
                 <li>
-                  <a href="">Game Nester</a>
+                  <a href=''>Game Nester</a>
                 </li>
                 <li>
-                  <a href="">Game CSS Diner</a>
+                  <a href=''>Game CSS Diner</a>
                 </li>
                 <li>
-                  <a href="">Game CSS Selectors</a>
+                  <a href=''>Game CSS Selectors</a>
                 </li>
                 <li>
-                  <a href="">Game Froggy</a>
+                  <a href=''>Game Froggy</a>
                 </li>
                 <li>
-                  <a href="">Game Froggy Pro</a>
+                  <a href=''>Game Froggy Pro</a>
                 </li>
                 <li>
-                  <a href="">Game Scoops</a>
+                  <a href=''>Game Scoops</a>
                 </li>
               </ul>
             </div>
           </div>
-          <div className="col" style={{ width: "16.66667%" }}>
-            <div style={{ paddingTop: "84px", fontWeight: "bold", paddingBottom: "15px" }}>
+          <div className='col' style={{ width: "16.66667%" }}>
+            <div
+              style={{
+                paddingTop: "84px",
+                fontWeight: "bold",
+                paddingBottom: "15px",
+              }}>
               CÔNG CỤ
             </div>
             <ul>
               <li>
-                <a href="">Tạo CV xin việc</a>
+                <a href=''>Tạo CV xin việc</a>
               </li>
               <li>
-                <a href="">Rút gọn liên kết</a>
+                <a href=''>Rút gọn liên kết</a>
               </li>
               <li>
-                <a href="">Clip-path maker</a>
+                <a href=''>Clip-path maker</a>
               </li>
               <li>
-                <a href="">Snippet generator</a>
+                <a href=''>Snippet generator</a>
               </li>
               <li>
-                <a href="">CSS Grid generator</a>
+                <a href=''>CSS Grid generator</a>
               </li>
               <li>
-                <a href="">Cảnh báo sờ tay lên mặt</a>
+                <a href=''>Cảnh báo sờ tay lên mặt</a>
               </li>
             </ul>
           </div>
-          <div className="col" style={{ width: "34%" }}>
+          <div className='col' style={{ width: "34%" }}>
             <div
               style={{
                 lineHeight: "26px",
                 fontWeight: "bold",
                 // paddingTop: "90px",
-              }}
-            >
+              }}>
               CÔNG TY CỔ PHẦN CÔNG <br /> NGHỆ GIÁO DỤC F8
             </div>
             <div
@@ -124,26 +141,21 @@ const Footer = () => {
                 paddingTop: "15px",
                 lineHeight: "30px",
                 fontSize: "15px",
-              }}
-            >
+              }}>
               <div>Mã số thuế: 0109922901</div>
               <div>Ngày thành lập: 04/03/2022</div>
               <div>
-                Lĩnh vực: Công nghệ, giáo dục, lập trình. <br />F8 xây dựng và
-                phát triển những sản <br /> phẩm mang lại giá trị cho cộng đồng.
+                Lĩnh vực: Công nghệ, giáo dục, lập trình. <br />
+                F8 xây dựng và phát triển những sản <br /> phẩm mang lại giá trị
+                cho cộng đồng.
               </div>
             </div>
           </div>
         </div>
         <div>
-          <span>
+          <p style={{ textAlign: "center", padding: "20px 0" }}>
             © 2018 - 2024 F8. Nền tảng học lập trình hàng đầu Việt Nam
-          </span>
-          <div>
-            <Button></Button>
-            <Button></Button>
-            <Button></Button>
-          </div>
+          </p>
         </div>
       </footer>
     </div>
--- src/components/layouts/LayoutWebsite.tsx
+++ src/components/layouts/LayoutWebsite.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../Header";
 import SideBar from "../SideBar";
@@ -6,6 +6,30 @@ import { Box } from "@mui/material";
 import Footer from "../Footer";
 
 const LayoutWebsite = () => {
+  const [scroll, setScroll] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+      const scrollHeight = document.documentElement.scrollHeight;
+      const clientHeight = document.documentElement.clientHeight;
+      const scrollPercentage =
+        (scrollTop / (scrollHeight - clientHeight)) * 100;
+
+      // Kiểm tra xem người dùng đã cuộn đến khoảng 95% chiều cao của trang chưa
+      if (scrollPercentage >= 85) {
+        setScroll(true);
+      } else {
+        setScroll(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <div>
       <Header />
@@ -15,8 +39,9 @@ const LayoutWebsite = () => {
         position={"fixed"}
         left={0}
         top={0}
-        paddingTop={"80px"}
+        marginTop={scroll ? "-20px" : "80px"}
         display={"flex"}
+        sx={{ transition: ".1s" }}
         flexDirection={"column"}
         alignItems={"center"}
         zIndex={2}>
