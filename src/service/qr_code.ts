f946fb390e5f385b4439eb7c607e8ef84d8caef3
crmi <crmi@localhost.localdomain>
2024-06-28 17:37:39 +0700

    qr code

---
+++ src/service/qr_code.ts
@@ -0,0 +1,18 @@
+import axios from "../core/api";
+export const getbanks = async () => {
+    try {
+      const response = await axios.get(`/qr_code/banks`);
+      return response;
+    } catch (error) {
+      console.log(`getbanks`, error);
+    }
+};
+
+export const generateQrCode = async (value: typeCourses) => {
+    try {
+      const response = await axios.post(`/qr_code/generate-qr`, value);
+      return response;
+    } catch (error) {
+      console.log(`generateQrCode`, error);
+    }
+  };
